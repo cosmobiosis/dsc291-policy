95f288d234b2a67110f184bddfb0466f23e94779
cosmobiosis <cosmobiosis@gmail.com>
2022-05-29 20:18:34 -0700

    feat: graph

--- analysis.py
+++ analysis.py
@@ -60,5 +60,51 @@ df5=(df4-df4.mean())/df4.std() # normalization
 print(df5)
 df5.dropna()
 df6 = clean_dataset(df5)
-mat = df5.values
-kmeans = KMeans(n_clusters=55, random_state=0).fit(mat)
+df5 = df5[["Latitude", "Longitude"]]
+df5 = df5.head(5000)
+reduced_data = df5.values
+kmeans = KMeans(n_clusters=31, random_state=0).fit(reduced_data)
+
+h = 0.02
+# Plot the decision boundary. For that, we will assign a color to each
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# Obtain labels for each point in mesh. Use last trained model.
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(
+    Z,
+    interpolation="nearest",
+    extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+    cmap=plt.cm.Paired,
+    aspect="auto",
+    origin="lower",
+)
+
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], "k.", markersize=2)
+# Plot the centroids as a white X
+centroids = kmeans.cluster_centers_
+plt.scatter(
+    centroids[:, 0],
+    centroids[:, 1],
+    marker="x",
+    s=20,
+    linewidths=1,
+    color="w",
+    zorder=10,
+)
+plt.title(
+    "K-means clustering on the crime-related geographical data\n"
+    "Centroids are marked with white cross"
+)
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
